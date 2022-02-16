c9bbe521778c8f581637e00ca62bd4041d9e19c3
kandalog <asuasu.tee@gmail.com>
2022-02-16 16:59:15 +0000

    Rails tutorial復習第10章　ユーザー削除直前まで

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,4 +43,17 @@ end
     @current_user = nil
   end
   
+  # 記憶したURL（もしくはデフォルト値）にリダイレクト
+def redirect_back_or(default)
+  redirect_to(session[:forwarding_url] || default)
+  session.delete(:forwarding_url)
+end
+
+# アクセスしようとしたURLを覚えておく
+def store_location
+# request.original_urlでリクエスト先のURLを取得できる
+# getのみ記憶 patchのurlを記憶してしまうと、最悪エラーになる
+  session[:forwarding_url] = request.original_url if request.get?
+end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   before_save { self.email = email.downcase }
   validates :name,  presence: true,  length: { maximum: 50 }
