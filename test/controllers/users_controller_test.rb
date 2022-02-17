b847e8673bfc0b505d440ee92278924b45e60a32
kandalog <asuasu.tee@gmail.com>
2022-02-17 05:23:50 +0000

    10章完了git add . エラーで詰まったけどなんとか解決git add .

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -54,5 +54,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                             admin: true } }
     assert_not @other_user.admin?
   end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
 
 end
