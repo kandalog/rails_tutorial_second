92dcfbe2ca847215f268869ce101fbc71d924e09
kandalog <asuasu.tee@gmail.com>
2022-03-06 14:05:19 +0000

    sampleユーザーでのログイン願いを追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,31 @@
-# ユーザー
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin:     true,
+             admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
+User.create!(name:  "Sample user",
+             email: "sample12496user@gmail.com	",
+             password:              "password",
+             password_confirmation: "password",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
 
-# マイクロポスト
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
+User.create!(name:  "田中",
+             email: "tanaka124sample13@gmail.com	",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
 
-# 以下のリレーションシップを作成する
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+User.create!(name:  "山田",
+             email: "yamada125823sample@gmail.com	",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
